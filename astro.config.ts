422644f1cbe131fb8642cfab2620bd125dc3ad39
lv123123long <a1749907938@outlook.com>
2024-08-20 00:10:52 +0800

    20240820init

---
+++ astro.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
+import { THEME_CONFIG } from "./src/theme.config";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://lv123123long.github.io/",
+  prefetch: true,
+  markdown: {
+    shikiConfig: {
+      theme: 'one-dark-pro',
+      langs: [],
+      wrap: true,
+    },
+  },
+  integrations: [
+    UnoCSS({
+      injectReset: true
+    }),
+    robotsTxt(),
+    sitemap(),
+    mdx()
+  ]
+});
